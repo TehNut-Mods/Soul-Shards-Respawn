af69c0c43693ad098b6763354bbb5e61c2bd5bc7
Nicholas Ignoffo <nick@tehnut.info>
2018-04-20 18:17:21 -0700

    Fix tinker's compatibilty

--- src/compat/java/info/tehnut/soulshardsrespawn/compat/tconstruct/TinkersCompatibilityPlugin.java
+++ src/compat/java/info/tehnut/soulshardsrespawn/compat/tconstruct/TinkersCompatibilityPlugin.java
@@ -11,6 +11,9 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.fluids.FluidRegistry;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -19,41 +22,51 @@ import slimeknights.tconstruct.library.TinkerRegistry;
 import slimeknights.tconstruct.library.Util;
 import slimeknights.tconstruct.library.client.MaterialRenderInfo;
 import slimeknights.tconstruct.library.client.texture.MetalTextureTexture;
+import slimeknights.tconstruct.library.fluid.FluidColored;
 import slimeknights.tconstruct.library.materials.*;
 import slimeknights.tconstruct.library.modifiers.Modifier;
 import slimeknights.tconstruct.library.traits.ITrait;
-import slimeknights.tconstruct.library.utils.HarvestLevels;
+import slimeknights.tconstruct.smeltery.TinkerSmeltery;
+
+import static slimeknights.tconstruct.library.utils.HarvestLevels.DIAMOND;
 
 @CompatibilityPlugin("tconstruct")
 public class TinkersCompatibilityPlugin implements ICompatibilityPlugin {
 
     public static final Modifier MOD_SOUL_STEALER = new ModifierSoulStealer();
     public static final ITrait TRAIT_VILE = new TraitVile();
+    public static final Fluid FLUID_CORRUPTED = new FluidColored(SoulShards.MODID + ".corrupted", 0x372458);
     public static final Material MATERIAL_CORRUPTED = new Material("corrupted", 0x7A10A7)
-            .setCastable(true)
             .addTrait(TRAIT_VILE, MaterialTypes.HEAD);
 
     @Override
     public void preInit() {
         MinecraftForge.EVENT_BUS.register(this);
-    }
+        FluidRegistry.registerFluid(FLUID_CORRUPTED);
+        MATERIAL_CORRUPTED.setFluid(FLUID_CORRUPTED);
+        MATERIAL_CORRUPTED.setCastable(true);
+        MATERIAL_CORRUPTED.setCraftable(false);
 
-    @SubscribeEvent
-    public void registerItems(RegistryEvent.Register<Item> event) {
         TinkerRegistry.addMaterial(MATERIAL_CORRUPTED);
         TinkerRegistry.addMaterialStats(
                 MATERIAL_CORRUPTED,
-                new HeadMaterialStats(204, 7.00F, 4.00F, HarvestLevels.IRON),
-                new HandleMaterialStats(0.85F, 60),
+                new HeadMaterialStats(204, 6.00f, 4.00f, DIAMOND),
+                new HandleMaterialStats(0.85f, 60),
                 new ExtraMaterialStats(50)
         );
     }
 
+    @SubscribeEvent(priority = EventPriority.HIGH)
+    public void registerItems(RegistryEvent.Register<Item> event) {
+    }
+
     @SubscribeEvent
     public void registerRecipes(RegistryEvent.Register<IRecipe> event) {
+        TinkerRegistry.registerMelting(Materials.CORRUPTED_INGOT.getStack(), FLUID_CORRUPTED, Material.VALUE_Ingot);
+        TinkerSmeltery.registerToolpartMeltingCasting(MATERIAL_CORRUPTED);
         MOD_SOUL_STEALER.addItem(Materials.CORRUPTED_INGOT.getStack(), 16, 1);
 
-        MATERIAL_CORRUPTED.addItem(Materials.CORRUPTED_INGOT.getStack(), 1, 1);
+        MATERIAL_CORRUPTED.addItem("ingotCorrupted", 1, Material.VALUE_Ingot);
         MATERIAL_CORRUPTED.setRepresentativeItem(Materials.CORRUPTED_INGOT.getStack());
     }
 
