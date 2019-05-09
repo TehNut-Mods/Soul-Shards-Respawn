1decea16db768c798537f8d3bc28565cf77dcecf
Draylar <samuelhimes@gmail.com>
2019-05-09 12:00:43 -0500

    ported CageSpawnEvent event to new event system

--- src/main/java/info/tehnut/soulshards/api/CageSpawnEvent.java
+++ src/main/java/info/tehnut/soulshards/api/CageSpawnEvent.java
@@ -1,14 +1,25 @@
 package info.tehnut.soulshards.api;
 
+import net.fabricmc.fabric.api.event.Event;
+import net.fabricmc.fabric.api.event.EventFactory;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 
-import java.util.ArrayList;
-
 public interface CageSpawnEvent {
 
-    ArrayList<CageSpawnEvent> CAGE_SPAWN = new ArrayList<>();
+    public static final Event<CageSpawnEvent> CAGE_SPAWN = EventFactory.createArrayBacked(CageSpawnEvent.class,
+            (listeners) -> (binding, shardStack, toSpawn) -> {
+                for(CageSpawnEvent event : listeners) {
+                    ActionResult result = event.onCageSpawn(binding, shardStack, toSpawn);
+                    if(result != ActionResult.PASS) {
+                        return result;
+                    }
+                }
+
+                return ActionResult.PASS;
+            }
+            );
 
     ActionResult onCageSpawn(IBinding binding, ItemStack shardStack, LivingEntity toSpawn);
 }
