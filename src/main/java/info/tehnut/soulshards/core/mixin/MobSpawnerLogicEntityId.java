cc73df5664cad9e83c2aca620180a029b017c02e
Draylar <samuelhimes@gmail.com>
2019-05-10 12:27:22 -0500

    remove nullable

--- src/main/java/info/tehnut/soulshards/core/mixin/MobSpawnerLogicEntityId.java
+++ src/main/java/info/tehnut/soulshards/core/mixin/MobSpawnerLogicEntityId.java
@@ -1,6 +1,5 @@
 package info.tehnut.soulshards.core.mixin;
 
-import com.sun.istack.internal.Nullable;
 import net.minecraft.util.Identifier;
 import net.minecraft.world.MobSpawnerLogic;
 import org.spongepowered.asm.mixin.Mixin;
@@ -8,7 +7,6 @@ import org.spongepowered.asm.mixin.gen.Invoker;
 
 @Mixin(MobSpawnerLogic.class)
 public interface MobSpawnerLogicEntityId {
-    @Nullable
     @Invoker("getEntityId")
     Identifier getEntityId();
 }
